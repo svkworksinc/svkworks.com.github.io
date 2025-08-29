b585551c6e36198260c8a0dc6c52ab475a36be04
svkworksinc <svk.works.inc@gmail.com>
2025-08-29 00:13:22 -0500

    Changed entire website

--- cart.js
+++
@@ -1,8 +0,0 @@
-function updateCartCount() {
-  let cart = JSON.parse(localStorage.getItem("cart")) || [];
-  let count = cart.reduce((sum, item) => sum + item.quantity, 0);
-  document.getElementById("cartCount").innerText = count;
-}
-
-// Run when page loads
-document.addEventListener("DOMContentLoaded", updateCartCount);
